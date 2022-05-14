9836ae9798a6d4d9f423e489af095918802a16a8
TahminaM <Tahminamunni07@gmail.com>
2022-05-14 18:08:33 -0400

    Edit campus updates

--- src/components/containers/EditCampusContainer.js
+++ src/components/containers/EditCampusContainer.js
@@ -1,4 +1,4 @@
-import Header from './Header';
+//import Header from './Header';
 import { Component } from 'react';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
@@ -37,6 +37,69 @@ class EditCampusContainer extends Component {
             name: this.state.name,
             address: this.state.address,
             description: this.state.description,
-            //imageUrl: this.state.imageUrl,
-            id: this.props.campus.id
+            imageUrl: this.state.imageUrl,
+            id: this.props.campus.id //id or campusID
         };
+
+        console.log("campus: ")
+        console.log(campus)
+
+        console.log("this.props.campus: ")
+        console.log(this.props.campus)
+
+        let newCampus = await this.props.editCampus(campus);
+
+        console.log("newCampus: ")
+        console.log(newCampus)
+
+        this.setState({
+            name: "", 
+            address: "", 
+            description: "",
+            imageUrl: 0,
+            redirect: true, 
+            redirectId: this.props.campus.id
+          });
+      }
+  
+      componentWillUnmount() {
+          this.setState({redirect: false, redirectId: null});
+      }
+
+      render() {
+
+        if(this.state.redirect) {
+          return (<Redirect to={`/campus/${this.state.redirectId}`}/>)
+        }
+        return (
+            
+            <EditCampusView 
+            campus={this.props.campus}
+            editCampus={this.props.editCampus}
+            deleteCampus={this.props.deleteCampus}
+            fetchCampus={this.props.fetchCampus}
+            handleChange = {this.handleChange} 
+            handleSubmit={this.handleSubmit}      
+          />
+        );
+    }
+}
+
+const mapState = (state) => {
+    return {
+      campus: state.campus,
+    };
+  };
+
+const mapDispatch = (dispatch) => {
+    return({
+        fetchCampus: (id) => dispatch(fetchCampusThunk(id)),
+        editCampus: (campus) => dispatch(editCampusThunk(campus))
+    })
+}
+
+export default connect(mapState, mapDispatch)(EditCampusContainer);
+
+  
+
+
