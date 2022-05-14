cbc8663a6cab49903670fc67582b8859bc158aa6
Ramon Torres <RTorres5318@gmail.com>
2022-05-14 12:37:55 -0400

    Can successfully add new students to db.

--- src/components/containers/NewStudentContainer.js
+++ src/components/containers/NewStudentContainer.js
@@ -19,7 +19,8 @@ class NewStudentContainer extends Component {
     super(props);
     this.state = {
       firstname: "", 
-      lastname: "", 
+      lastname: "",
+      email: "",
       campusId: null, 
       redirect: false, 
       redirectId: null
@@ -40,6 +41,7 @@ class NewStudentContainer extends Component {
     let student = {
         firstname: this.state.firstname,
         lastname: this.state.lastname,
+        email: this.state.email,
         campusId: this.state.campusId
     };
     
@@ -49,7 +51,8 @@ class NewStudentContainer extends Component {
     // Update state, and trigger redirect to show the new student
     this.setState({
       firstname: "", 
-      lastname: "", 
+      lastname: "",
+      email: "",
       campusId: null, 
       redirect: true, 
       redirectId: newStudent.id
