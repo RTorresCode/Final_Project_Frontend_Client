27ed4905e62c6b884316c86f938592fe272ab32d
TahminaM <Tahminamunni07@gmail.com>
2022-05-14 18:21:50 -0400

    EditStudents container  updates

--- src/components/views/EditStudentView.js
+++ src/components/views/EditStudentView.js
@@ -53,4 +53,136 @@ const useStyles = makeStyles(() => ({
     },
   }));
 
+  const EditStudentView = (props) => {
+    // console.log(props)
+    const { handleChange, handleSubmit } = props;
+    const classes = useStyles();
   
+    return (
+      <div className={classes.root}>
+        <AppBar position="static" elevation={0} className={classes.appBar}>
+          <Toolbar>
+            <Typography variant="h6" className={classes.title} color="#9ebe35">
+              Campus Manager
+            </Typography>
+  
+            <Link className={classes.links} to={"/"}>
+              <Button
+                variant="contained"
+                color="white"
+                style={{ marginRight: "10px", height: "60px", width: "200px" }}
+              >
+                Home
+              </Button>
+            </Link>
+  
+            <Link className={classes.links} to={"/campuses"}>
+              <Button
+                variant="contained"
+                color="white"
+                style={{ marginRight: "10px", height: "60px", width: "200px" }}
+              >
+                All Campuses
+              </Button>
+            </Link>
+  
+            <Link className={classes.links} to={"/students"}>
+              <Button
+                variant="contained"
+                color="white"
+                style={{ marginRight: "10px", height: "60px", width: "200px" }}
+              >
+                All Students
+              </Button>
+            </Link>
+          </Toolbar>
+        </AppBar>
+  
+        <div className={classes.formContainer}>
+          <div className={classes.formTitle}>
+            <Typography
+              style={{
+                fontWeight: "bold",
+                fontFamily: "Courier, sans-serif",
+                fontSize: "20px",
+                color: "#11153e",
+              }}
+            >
+              Edit Student
+            </Typography>
+          </div>
+          <form style={{ textAlign: "center" }} onSubmit={(e) => handleSubmit(e)}>
+            <label style={{ color: "#11153e", fontWeight: "bold" }}>
+              First Name:{" "}
+            </label>
+            <input
+              type="text"
+              name="firstname"
+              onChange={(e) => handleChange(e)}
+              defaultValue={props.student.firstname}
+              required
+            />
+            <br />
+            <br />
+  
+            <label style={{ color: "#11153e", fontWeight: "bold" }}>
+              Last Name:{" "}
+            </label>
+            <input
+              type="text"
+              name="lastname"
+              onChange={(e) => handleChange(e)}
+              defaultValue={props.student.lastname}
+              required
+            />
+            <br />
+            <br />
+  
+            <label style={{ color: "#11153e", fontWeight: "bold" }}>
+              campusId:{" "}
+            </label>
+            <input
+              type="text"
+              name="campusId"
+              onChange={(e) => handleChange(e)}
+              defaultValue={props.student.campusId}
+            />
+            <br />
+            <br />
+  
+            <label style={{ color: "#11153e", fontWeight: "bold" }}>
+              Email:{" "}
+            </label>
+            <input
+              type="text"
+              name="email"
+              onChange={(e) => handleChange(e)}
+              defaultValue={props.student.email}
+              required
+            />
+            <br />
+            <br />
+  
+            <label style={{ color: "#11153e", fontWeight: "bold" }}>GPA: </label>
+            <input
+              type="double"
+              name="gpa"
+              onChange={(e) => handleChange(e)}
+              defaultValue={props.student.gpa}
+              required
+            />
+            <br />
+            <br />
+  
+            <Button variant="contained" color="primary" type="submit">
+              Submit
+            </Button>
+            <br />
+            <br />
+          </form>
+        </div>
+      </div>
+    );
+  };
+  
+  export default EditStudentView;
