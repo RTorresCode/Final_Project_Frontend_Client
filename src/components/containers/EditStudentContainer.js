ad2843466e791835f857c29968cb0749b1aa3838
TahminaM <Tahminamunni07@gmail.com>
2022-05-14 19:11:55 -0400

    Minor changes in code

--- src/components/containers/EditStudentContainer.js
+++ src/components/containers/EditStudentContainer.js
@@ -1,4 +1,3 @@
-//import Header from './Header';
 
 import { Component } from 'react';
 import { connect } from 'react-redux';
@@ -13,29 +12,125 @@ class EditStudentContainer extends Component {
     constructor(props){
         super(props);
         this.state = {
-          firstname: this.props.student.firstname, 
-          lastname: this.props.student.lastname, 
-          email: this.props.student.email,
-          gpa: this.props.student.gpa,
-          gpaError: "",
-          campusId: this.props.student.campusId, 
+          firstname: "", 
+          lastname: "",
+          gpa: -1,
+          email: "",
+          imageUrl: "",
+          id: -1,
+          campusId: -1, 
           redirect: false, 
+          errorCaught: false, 
           redirectId: null
         };
     }
 
     componentDidMount() {
-        //getting student ID from url
-        this.props.fetchStudent(this.props.match.params.id);
-      }
+      this.setState({firstname: this.props.student.firstname,
+          lastname: this.props.student.lastname, 
+          gpa: this.props.student.gpa,
+          email: this.props.student.email,
+          imageUrl: this.props.student.imageUrl,
+          campusId: this.props.student.campusId,
+          id: this.props.student.id});  
+    }
 
     handleChange = event => {
-      this.setState({
-        [event.target.name]: event.target.value},() => {
-          this.validateGPA();
-      });
-        console.log(this.state);
+      const {name, value, type, checked} = event.target
+      type === "checkbox" ? this.setState({ [name]: checked }) : this.setState({ [name]: value })
     }
+    handleSubmit = async event =>  {
+      event.preventDefault(); 
+  
+      let new_info = { 
+        firstname: this.state.firstname,
+        lastname: this.state.lastname,
+        gpa: this.state.gpa,
+        email: this.state.email,
+        imageUrl: this.state.imageUrl,
+        campusId: this.state.campusId,
+        id: this.state.id
+      };
+      if (new_info.imageUrl === "") { 
+      }
+  
+      
+        try {
+          let student = await this.props.editStudent(new_info) 
+          console.log(student.id); 
+          alert(`${new_info.firstname} ${new_info.lastname}'s edit was saved.`); 
+      
+          this.setState({
+            firstname: "", 
+            lastname: "",
+            gpa: -1,
+            email: "",
+            imageUrl: "",
+            id: -1,
+            campusId: -1,
+            redirect: true, 
+          });
+        }
+        catch(err) { 
+          console.error(err); 
+          alert("Error with edit! Please follow the Student Information guidelines found below");
+          this.setState({
+            errorCaught: true 
+          });
+        };
+    }
+    
+    componentWillUnmount() {
+      this.setState({redirect: false, redirectId: null});
+    }
+    
+    render() {
+      if(this.state.redirect) {
+        window.location.reload(); 
+      }
+  
+     return (
+        <div>
+          <EditStudentView
+            handleChange = {this.handleChange} 
+            handleSubmit={this.handleSubmit}
+            student={this.props.student}
+          />
+          {this.state.errorCaught ? (
+            <div>
+              <br />
+              <p>Student First and Last names: Cannot be null.</p>
+              <p>Student's Campus ID: Must be a valid  and actual campus ID of a school within this database.</p>
+              <p>Student Email: Cannot be null.</p>
+              <p>Student Image: Can be left blank.</p>
+              <p>Student GPA: Must be between 0.0 and 4.0.</p>
+            </div>
+          ) : (
+            null
+          )}
+        </div>          
+      );
+    }
+  }
+  
+ const mapDispatch = (dispatch) => {
+      return({
+          editStudent: (student) => dispatch(editStudentThunk(student)),
+      })
+  }
+  
+  export default connect(null, mapDispatch)(EditStudentContainer);
+
+
+
+
+
+
+
+
+
+
+
   //Checking the GPA and console logging the error if invalid
       validateGPA = () => {
         const { gpa } = this.state;
