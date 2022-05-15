6ece0b3e859202a871836afb9a7abee3818fc756
TahminaM <Tahminamunni07@gmail.com>
2022-05-14 22:24:47 -0400

    Cs update

--- src/components/views/AllStudentsView.js
+++ src/components/views/AllStudentsView.js
@@ -5,9 +5,11 @@ The Views component is responsible for rendering web page with data provided by 
 It constructs a React component to display the all students view page.
 ================================================== */
 import { Link } from "react-router-dom";
+import { EditStudentContainer } from "../containers/index"
+
 
 const AllStudentsView = (props) => {
-  const {students, deleteStudent} = props;
+  const {students, deleteStudent, toggleEdit, editing} = props;
   // If there is no student, display a message
   if (!students.length) {
     return (
@@ -25,24 +27,42 @@ const AllStudentsView = (props) => {
     <div>
       <h1>All Students</h1>
 
+      <Link to={`/newstudent`}>
+        <button>Add New Student</button>
+      </Link>
+      <br />
+      {editing ? (
+        <button onClick={toggleEdit}>Quit Editing</button>
+      ) : (
+        <button onClick={toggleEdit}>Edit Students</button>
+      )}
+      <br/><br/>
+
       {students.map((student) => {
           let name = student.firstname + " " + student.lastname;
+          let url = student.imageUrl;
           return (
             <div key={student.id}>
               <Link to={`/student/${student.id}`}>
                 <h2>{name}</h2>
               </Link>
+              <h3>{student.email}</h3>
+              {}
+              <img src={url} alt="Student Profile"/>
+              <br/>
               <button onClick={() => deleteStudent(student.id)}>Delete</button>
+              <br />
+              {editing ? (
+              <EditStudentContainer student={student}/>
+              ) : (
+              null // do nothing
+              )}
               <hr/>
             </div>
           );
         }
       )}
       <br/>
-      <Link to={`/newstudent`}>
-        <button>Add New Student</button>
-      </Link>
-      <br/><br/>
     </div>
   );
 };
