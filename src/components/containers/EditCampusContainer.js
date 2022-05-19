103942c187ceac3b9ad738d98891cc07d156a349
TahminaM <Tahminamunni07@gmail.com>
2022-05-18 21:26:27 -0400

    Updated code for edit students and campus information

--- src/components/containers/EditCampusContainer.js
+++ src/components/containers/EditCampusContainer.js
@@ -1,8 +1,11 @@
-
+import Header from './Header';
 import { Component } from 'react';
 import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+
 import EditCampusView from '../views/EditCampusView';
 import { editCampusThunk } from '../../store/thunks';
+import { fetchCampusThunk } from "../../store/thunks";
 
 
 class EditCampusContainer extends Component {
